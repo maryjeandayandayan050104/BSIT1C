0182c0d904194b9e6669b5ebe70a50af5df4a90f
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2022-09-28 10:05:26 +0800

    Add files via upload

---
+++ registrationform.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define p printf
+int main () {
+    p("---------- REGISTRATION FORM ---------- \n\n");
+    p("~~~~~~~~~ PERSONAL INFORMATION ~~~~~~~ \n");
+    p("NAME: \n");
+    p("STUDENT #: \n");
+    p("ADDRESS: \n");
+    p("AGE: \n");
+    p("GENDER: \n");
+    p("CIVIL STATUS: \n");
+    p("HEIGHT: \n");
+    p("WEIGHT: \n");
+    p("DATE OF BIRTH: \n");
+    p("PLACE OF BIRTH: \n");
+    p("RELIGION: \n");
+    p("CONTACT NUMBER: \n");
+    p("EMAIL: \n\n\n");
+    p("~~~~~~~~~ EDUCATIONAL ATTAINMENT ~~~~~~~ \n");
+    p("LAST SCHOOL ATTENDED: \n");
+    p("STRAND: \n");
+    p("CURRENT SCHOOL: \n");
+    p("1ST CHOICE: \n");
+    p("SECOND CHOICE: \n");
+    p("THIRD CHOICE: \n\n\n");
+    p("------------------ END ----------------- \n\n");
+
+
+    return 0;
+
+}
