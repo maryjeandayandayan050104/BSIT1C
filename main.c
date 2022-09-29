a21582d5df045b6defe958587b6dacccdfa86310
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2022-09-29 11:53:58 +0800

    Add files via upload
    ACTIVITY1-1C-DAYANDAYAN

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define p printf
+
+int main()
+{
+    p("------------------------- MY PROFILE ----------------------\n\n");
+    p("NAME: MARY JEAN CAYLAN DAYANDAYAN \n");
+    p("AGE: 18 \n");
+    p("BIRTHDAY: MAY 01, 2004 \n");
+    p("ADDRESS: SOUTHVILLE 8C, MONTALBAN, RODRIGUEZ RIZAL \n");
+    p("CONTACT NO.: 09452055282 \n");
+    p("EMAIL: dayandayanmaryjean@gmail.com \n");
+    p("CITIZENSHIP: Filipino \n");
+    p("FATHER'S NAME: EMERITO BACULI DAYANDAYAN \n");
+    p("MOTHER'S NAME: EVANGELINE CAYLAN DAYANDAYAN \n");
+
+    return 0;
+}
