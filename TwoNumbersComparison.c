398592965a929ad5383211f7076facd7de38c35d
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2022-11-09 18:10:25 +0800

    Add files via upload

---
+++ TwoNumbersComparison.c
@@ -0,0 +1,27 @@
+
+// Two Numbers Comparison
+// Dayandayan, Mary Jean C
+// BSIT 1C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int a, b;
+
+    printf("Enter value for a: ");
+    scanf("%d", &a);
+    printf("Enter value for b: ");
+    scanf("%d", &b);
+
+    if(a>b){
+    printf("A>B\n");
+    }
+
+    if(b>a){
+    printf("B>A\n");
+    }
+
+    return 0;
+}
