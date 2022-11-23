7cbf37514ccc44ddebd0ffe2467c30a3a8ab2b84
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2022-11-23 22:33:34 +0800

    Add files via upload

---
+++ relational-operator.c
@@ -0,0 +1,35 @@
+// DAYANDAYAN, MARY JEAN C.
+// BSIT 1C
+// RELATIONAL OPERATOR
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int x = 10;
+    int y = 5;
+
+    if(x==y){
+        printf("Both variables are equal\n");
+    }
+    if(x>y){
+        printf("x is greater than y\n");
+    }
+    if(x<y){
+        printf("x is less than y\n");
+    }
+    if(x!=y){
+        printf("x is not equal to y\n");
+    }
+    if(x<=y){
+        printf("x is lesser or equal to y\n");
+    }
+    if(x>=y){
+        printf("x is greater or equal to y\n");
+    }
+    printf("CODES AFTER IF!");
+
+
+    return 0;
+}
