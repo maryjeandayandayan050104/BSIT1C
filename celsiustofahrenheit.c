89d65bc69dd13a30ae02061cbac84383a2c85450
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2023-07-06 12:58:13 +0800

    Add files via upload

--- celsiustofahrenheit.c
+++ celsiustofahrenheit.c
@@ -1,26 +1,48 @@
 /*
-DAYANDAYAN, MARY JEAN C.
-BSIT 1C
-CELSIUS TO FAHRENHEIT
+    DAYANDAYAN, MARY JEAN C.
+    BSIT 1C
+    FINALS INDIVIDUAL PROJECT
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#define p printf
-#define s scanf
-  
+
+    char convert, again;
     float c, f;
 
 int main() {
 
-p("Enter Celsius: ");
-s( "%f", &c);
+    do{
+    system("cls");
+    printf("\nDo you want to try out the 'CELSIUS TO FAHRENHEIT CONVERSION' program? [Y/N]:  ");
+    scanf("%s", &convert);
 
-f = (c*9/5)+32;
-system("cls");
+        if(convert == 'y' || convert == 'Y'){
+            system("cls");
+            printf("\n\n    Enter Celsius: ");
+            scanf( "%f", &c);
 
-p("%.1f Celsius = %.1f in Fahrenheit", c, f);
+            f = (c*9/5)+32;
+            system("cls");
 
-return 0;
+            printf("___________________________________________________\n\n");
+            printf("   %.1f Celsius = %.1f Fahrenheit\n\n", c, f);
+            printf("___________________________________________________\n\n");
 
+        }else if(convert == 'n' || convert == 'N'){
+            return 0;
+        }else{
+            main ();
+        }
+
+            printf("Do you want to use this function again? [Y/N]:  ");
+            scanf("%s", &again);
+
+        }while(again == 'y' || again == 'Y');
+        if(again == 'n' || again == 'N'){
+            return 0;
+        }else{
+            main();
+        }
+    return 0;
 }
